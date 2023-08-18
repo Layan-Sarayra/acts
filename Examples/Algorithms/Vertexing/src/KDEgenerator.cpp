0037c9d96237538d85702ad5bd77b7cdf9e1c738
Layan Sarayra <layansarayrah@gmail.com>
2023-08-18 15:31:36 +0200

    changes to kde files

--- Examples/Algorithms/Vertexing/src/KDEgenerator.cpp
+++ Examples/Algorithms/Vertexing/src/KDEgenerator.cpp
@@ -4,39 +4,50 @@
 #include <TFile.h>
 #include <TTree.h>
 
-ActsExamples::KDEAlgorithm::KDEAlgorithm(const Config& cfg,
-					 Acts::Logging::Level lvl)
-  : ActsExamples::IAlgorithm("KDEAlgorithm", lvl), m_cfg(cfg) {}
+using namespace std;
 
-ActsExamples::ProcessCode ActsExamples::KDEAlgorithm::execute(const ActsExamples::AlgorithmContext& ctx) const {
-    
-  ///ACTS_INFO(m_cfg.message);
+namespace ActsExamples {
 
+KDEAlgorithm::KDEAlgorithm(const Config& cfg, Acts::Logging::Level lvl)
+    : IAlgorithm("KDEAlgorithm", lvl), m_cfg(cfg) {}
+
+ProcessCode KDEAlgorithm::execute(const AlgorithmContext& ctx) const {
+
+    ///ACTS_INFO(m_cfg.message);
   const char* rootFilePath = "/eos/user/l/lalsaray/odd_output/tracksummary_ambi.root";
   TFile* inputFile = TFile::Open(rootFilePath);
-  if (!inputFile || inputFile->IsZombie()) {
-    ACTS_ERROR("Error opening ROOT file");
-    return ActsExamples::ProcessCode::ABORT;
-  }
+  // if (!inputFile || inputFile->IsZombie()) {
+  //     ACTS_ERROR("Error opening ROOT file");
+  //     return ActsExamples::ProcessCode::ABORT;
+  // }
 
   TTree* inputTree = dynamic_cast<TTree*>(inputFile->Get("tracksummary"));
-  if (!inputTree) {
-    ACTS_ERROR("Error getting TTree from ROOT file");
-    inputFile->Close();
-    return ActsExamples::ProcessCode::ABORT;
-  }
+  // if (!inputTree) {
+  //     ACTS_ERROR("Error getting TTree from ROOT file");
+  //     inputFile->Close();
+  //     return ActsExamples::ProcessCode::ABORT;
+  // }
 
   std::vector<float> err_eLOC0_fit;
   TBranch* br_err_eLOC0_fit = inputTree->GetBranch("err_eLOC0_fit");
   br_err_eLOC0_fit->SetAddress(&err_eLOC0_fit);
 
-  Long64_t nEntries = inputTree->GetEntries();
-  for (Long64_t i = 0; i < nEntries; ++i) {
-    br_err_eLOC0_fit->GetEntry(i);
-    ACTS_INFO("Entry " << i << ": err_eLOC0_fit = " << err_eLOC0_fit[0]);
-  }
+  Long64_t entry = 0;  // Change this to the desired event number
+  inputTree->GetEntry(entry);
+
+  ACTS_INFO("Entry " << entry << ": err_eLOC0_fit = " << err_eLOC0_fit[0]);
 
   inputFile->Close();
 
-  return ActsExamples::ProcessCode::SUCCESS;
+  return ProcessCode::SUCCESS;
+}
+
+}
+
+std::array<Kernel,2> const ActsExamples::KDEAlgorithm :: getKDE_from_PDF() const{
+  std::array<Kernel,2> kernels;
+
+
+
+  return kernels;    
 }
