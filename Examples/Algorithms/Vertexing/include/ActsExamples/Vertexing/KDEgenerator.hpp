e4415b1bea730a74f7a873fa366f9011caf8eeff
Layan Sarayra <layansarayrah@gmail.com>
2023-08-23 16:29:20 +0200

    kde py code give *** Break *** segmentation violation

--- Examples/Algorithms/Vertexing/include/ActsExamples/Vertexing/KDEgenerator.hpp
+++ Examples/Algorithms/Vertexing/include/ActsExamples/Vertexing/KDEgenerator.hpp
@@ -1,4 +1,4 @@
-#pragma once /* to ensure that this file is only included -read- once even if code executed itirates in a single compilation */
+#pragma once // Ensure this file is included only once during compilation
 
 #include "ActsExamples/Framework/DataHandle.hpp"
 #include "ActsExamples/Framework/IAlgorithm.hpp"
@@ -8,25 +8,26 @@
 #include <memory>
 #include <string>
 
-
 namespace ActsExamples {
 
   class KDEAlgorithm final : public IAlgorithm {
   public:
-    struct Config { /// defines a nested structure within the class to group related configuration options
-      /// The message we are going to log.
+    struct Config {
+      // Define configuration options here if needed
       std::string message;
     };
-    ///logging: record information and events during execution by sending (info, warning, error, etc) to console -output- 
+
+    // Constructor
     KDEAlgorithm(const Config& cfg, Acts::Logging::Level lvl);
 
-    /// This function will be called on each event by the sequencer. (coded in IAlgorithm -> AlgorithmContext)
+    // This function will be called on each event by the sequencer
     ProcessCode execute(const AlgorithmContext& ctx) const final;
 
+    // Accessor for configuration
     const Config& config() const { return m_cfg; }
 
   private:
-    Config m_cfg;
+    Config m_cfg; // Configuration data for the algorithm
   };
 
-}
+} // namespace ActsExamples
--- Examples/Algorithms/Vertexing/src/KDEgenerator.cpp
+++ Examples/Algorithms/Vertexing/src/KDEgenerator.cpp
@@ -1,11 +1,16 @@
 #include "ActsExamples/Vertexing/KDEgenerator.hpp"
 #include "VertexingHelpers.hpp"
 #include "Acts/Utilities/Logger.hpp"
+#include <iostream>
+#include <fstream> // Include for file output
+#include <vector>
+#include <cmath>
 #include <TFile.h>
 #include <TTree.h>
-#include <TH1F.h>
+#include <TBranch.h>
 
-using namespace std;
+// Include the header for Long64_t
+#include <TTree.h>
 
 namespace ActsExamples {
 
@@ -18,55 +23,82 @@ ProcessCode KDEAlgorithm::execute(const AlgorithmContext& ctx) const {
     const char* rootFilePath = "/eos/user/l/lalsaray/odd_output/tracksummary_ambi.root";
 
     TFile* inputFile = TFile::Open(rootFilePath);
+    std::cout << "I broke here :) 1" << std::endl;
     if (!inputFile || inputFile->IsZombie()) {
         ACTS_ERROR("Error opening ROOT file");
         return ActsExamples::ProcessCode::ABORT;
     }
 
     TTree* inputTree = dynamic_cast<TTree*>(inputFile->Get("tracksummary"));
+    std::cout << "I broke here :) 2" << std::endl;
     if (!inputTree) {
         ACTS_ERROR("Error getting TTree from ROOT file");
         inputFile->Close();
         return ActsExamples::ProcessCode::ABORT;
     }
 
-    std::vector<float> err_eLOC0_fit;  // chose data/branch needed
-    
+    // Define variables to store data from the branches of interest
+    std::vector<float> err_eLOC0_fit;
+
     TBranch* br_err_eLOC0_fit = inputTree->GetBranch("err_eLOC0_fit");
+    std::cout << "I broke here :) 3" << std::endl;
+    if (!br_err_eLOC0_fit) {
+        ACTS_ERROR("Branch 'err_eLOC0_fit' not found in the ROOT file");
+        inputFile->Close();
+        return ActsExamples::ProcessCode::ABORT;
+    }
     br_err_eLOC0_fit->SetAddress(&err_eLOC0_fit);
+    std::cout << "I broke here :) 4" << std::endl;
 
     // Define KDE parameters
     double bandwidth = 1.0; // You can adjust this value based on your data
 
-    // Create a histogram to store the KDE results
-    TH1F* kdeHistogram = new TH1F("kdeHistogram", "Kernel Density Estimation", 100, -5.0, 5.0);
+    // Open a text file to save the KDE values
+    std::ofstream outputFile("KDE_values.txt");
+    std::cout << "I broke here :) 5" << std::endl;
+    if (!outputFile.is_open()) {
+        ACTS_ERROR("Error opening text file for output");
+        inputFile->Close();
+        return ActsExamples::ProcessCode::ABORT;
+    }
 
     // Loop over the entries and perform calculations
     for (Long64_t entry = 0; entry < inputTree->GetEntries(); ++entry) {
         inputTree->GetEntry(entry);
+        std::cout << "I broke here :) 6" << std::endl;
 
+        // Check if err_eLOC0_fit vector is empty
+        if (err_eLOC0_fit.empty()) {
+            std::cout << "err_eLOC0_fit is empty for entry " << entry << ", skipping the loop." << std::endl;
+            continue;  // Skip this iteration of the loop
+        }
+
+        // Use the branches of interest in your KDE calculation here
         for (size_t i = 0; i < err_eLOC0_fit.size(); ++i) {
+            // You can access the relevant branches (eLOC0_fit, eLOC1_fit, etc.) here
             double dataPoint = err_eLOC0_fit[i];
 
             double kdeValue = 0.0;
+            std::cout << "I broke here :) 7" << std::endl;
+
+            // Adjusted to only one loop instead of two
             for (size_t j = 0; j < err_eLOC0_fit.size(); ++j) {
                 double diff = (dataPoint - err_eLOC0_fit[j]) / bandwidth;
                 double kernel = exp(-0.5 * diff * diff) / (sqrt(2 * M_PI) * bandwidth);
                 kdeValue += kernel;
             }
-            kdeHistogram->Fill(dataPoint, kdeValue);
+            std::cout << "I broke here :) 8" << std::endl;
+
+            // Save the KDE value to the text file
+            outputFile << kdeValue << "\n";
+            std::cout << "I broke here :) 10" << std::endl;
         }
     }
 
-    // Now, `kdeHistogram` contains the KDE estimate of the data
+    // Close the text file
+    outputFile.close();
 
-    // Save the histogram
-    TFile* outputFile = TFile::Open("/eos/user/l/lalsaray/KDE_output.root", "RECREATE");
-    kdeHistogram->Write();
-    outputFile->Close();
-
-    //clean up memory by closing the ROOT file
-    delete kdeHistogram;
+    // Clean up memory by closing the ROOT file
     inputFile->Close();
 
     return ActsExamples::ProcessCode::SUCCESS;
