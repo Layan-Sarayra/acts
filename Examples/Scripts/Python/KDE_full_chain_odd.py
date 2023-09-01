a3ed7dfcc540fc9ca622706e173bfdf4b196d034
Layan Sarayra <layansarayrah@gmail.com>
2023-09-01 20:06:22 +0200

    KDE py file

--- Examples/Scripts/Python/KDE_full_chain_odd.py
+++ Examples/Scripts/Python/KDE_full_chain_odd.py
@@ -1,14 +1,15 @@
 #!/usr/bin/env python3
 import os, argparse, pathlib, acts, acts.examples
 from pathlib import Path
-from typing import Optional, Union, List
+from typing import Optional, Union
 
 parser = argparse.ArgumentParser(description="Full chain with the OpenDataDetector")
 parser.add_argument("--events", "-n", help="Number of events", type=int, default=10)
 args = vars(parser.parse_args())
 
 outputDir = pathlib.Path("/eos/user/l/lalsaray/KDE_output")
-s = acts.examples.Sequencer(events=args["events"], numThreads=1, outputDir=str(outputDir),)
+
+s = acts.examples.Sequencer(events=args["events"], numThreads=1, outputDir=str(outputDir))
 
 @acts.examples.NamedTypeArgs(logLevel=acts.logging.Level)
 def KDE_printer(
@@ -16,9 +17,7 @@ def KDE_printer(
     logLevel: Optional[acts.logging.Level] = None,
     # outputDirRoot: Optional[Union[Path, str]] = None,   
 ) -> None:
-    
     from acts.examples import KDEAlgorithm
-
     customLogLevel = acts.examples.defaultLogging(s, logLevel)
 
     KDE = KDEAlgorithm(level = customLogLevel())
@@ -28,11 +27,10 @@ def KDE_printer(
 
 if __name__ == "__main__":
     args = vars(parser.parse_args())
-    outputDir = pathlib.Path("/eos/user/l/lalsaray/KDE_output")
     os.environ["ACTS_SEQUENCER_DISABLE_FPEMON"] = "1"
 
     s = acts.examples.Sequencer(events=args["events"], numThreads=1, outputDir=str(outputDir))
 
-    s = KDE_printer(s)  # Add KDE printing to sequencer
+    s = KDE_printer(s)  # Add KDE printing to the sequencer
     
     s.run()    # Execute the sequencer
